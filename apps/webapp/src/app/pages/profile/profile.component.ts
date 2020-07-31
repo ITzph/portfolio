7e890afc7ec6b37dabc1e72b1dd5b6af2a62bb4c
carlogino <carloginocatapang@gmail.com>
2020-07-31 21:34:28 +0800

    Added scss style in libs

--- apps/webapp/src/app/pages/profile/profile.component.ts
+++ apps/webapp/src/app/pages/profile/profile.component.ts
@@ -9,7 +9,7 @@ import { loadProfiles } from '../../actions/profile.actions';
 @Component({
   selector: 'portfolio-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css'],
+  styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent implements OnInit {
   currentUser$: Observable<Profile>;
