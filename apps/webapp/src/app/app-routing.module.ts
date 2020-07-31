94fc5e30a15de5f56b175601108d98391cd74398
carlogino <carloginocatapang@gmail.com>
2020-07-31 12:36:44 +0800

    Updated commit hooks

--- apps/webapp/src/app/app-routing.module.ts
+++ apps/webapp/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'profile',
+    loadChildren: () => import('./profile/profile.module').then((m) => m.ProfileModule),
+  },
+];
 
 @NgModule({
   imports: [
