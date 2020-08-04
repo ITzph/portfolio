2aff820b99306a9b45e332807b66aa67ac305b3f
carlogino <carloginocatapang@gmail.com>
2020-08-04 20:51:48 +0800

    Added environtment file

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProfileModule } from './profile/profile.module';
 import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ProfileModule, DatabaseModule],
+  imports: [ProfileModule, DatabaseModule, ConfigModule.forRoot({})],
   controllers: [AppController],
   providers: [AppService],
 })
--- apps/api/src/app/database/database.module.ts
+++ apps/api/src/app/database/database.module.ts
@@ -1,19 +1,28 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mariadb',
-      host: process.env.PORTFOLIO_DB_HOST,
-      port: Number.parseInt(process.env.PORTFOLIO_DB_PORT, 10),
-      username: process.env.PORTFOLIO_DB_USERNAME,
-      password: process.env.PORTFOLIO_DB_PASSWORD,
-      database: process.env.PORTFOLIO_DB_DATABASE,
-      entities: [User],
-      synchronize: false,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        return {
+          type: configService.get<string>('PORTFOLIO_DB_TYPE') as any,
+          host: configService.get<string>('PORTFOLIO_DB_HOST'),
+          port: Number.parseInt(configService.get<string>('PORTFOLIO_DB_PORT'), 10),
+          username: configService.get<string>('PORTFOLIO_DB_USERNAME'),
+          password: configService.get<string>('PORTFOLIO_DB_PASSWORD'),
+          database: configService.get<string>('PORTFOLIO_DB_DATABASE'),
+          entities: [User],
+          synchronize: false,
+        };
+      },
+      inject: [ConfigService],
     }),
   ],
 })
-export class DatabaseModule {}
+export class DatabaseModule {
+  constructor() {}
+}
