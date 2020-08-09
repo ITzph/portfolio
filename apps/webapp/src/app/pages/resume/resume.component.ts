230f2b435bc5b6017995995bbab4350e0b86529e
carlogino <carloginocatapang@gmail.com>
2020-08-09 11:52:31 +0800

    Added resume module

---
+++ apps/webapp/src/app/pages/resume/resume.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'portfolio-resume',
+  templateUrl: './resume.component.html',
+  styleUrls: ['./resume.component.scss'],
+})
+export class ResumeComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
---
+++ apps/webapp/src/app/pages/resume/resume.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ResumeRoutingModule } from './resume-routing.module';
+import { ResumeComponent } from './resume.component';
+
+@NgModule({
+  declarations: [ResumeComponent],
+  imports: [CommonModule, ResumeRoutingModule],
+})
+export class ResumeModule {}
