a6f8f2013a869a95bcb4815c93d8ce5fe6854aea
carlogino <carloginocatapang@gmail.com>
2020-08-22 18:56:16 +0800

    Use sharingan in header implementation

--- apps/webapp/src/app/modules/header/header.component.ts
+++ apps/webapp/src/app/modules/header/header.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2, ElementRef } from '@angular/core';
+import { Subject } from 'rxjs';
+import { throttleTime } from 'rxjs/operators';
 
 @Component({
   selector: 'portfolio-header',
@@ -6,7 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-  constructor() {}
+  constructor(private el: ElementRef, private render: Renderer2) {}
 
-  ngOnInit(): void {}
+  isPinned = false;
+
+  scrollEvent: Subject<number> = new Subject();
+
+  ngOnInit() {
+    this.render.listen('body', 'wheel', () => {
+      const rect = this.el.nativeElement.getBoundingClientRect().top;
+      this.scrollEvent.next(rect);
+    });
+
+    this.scrollEvent.pipe(throttleTime(50)).subscribe((rect) => {
+      if (rect < -20) {
+        this.isPinned = true;
+      } else {
+        this.isPinned = false;
+      }
+    });
+  }
 }
