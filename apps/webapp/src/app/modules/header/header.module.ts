bf60a3a263b4f56e362008234d92c9de8bebf73f
carlogino <carloginocatapang@gmail.com>
2020-09-03 20:33:25 +0800

    Added hamburger icon

--- apps/webapp/src/app/modules/header/header.module.ts
+++ apps/webapp/src/app/modules/header/header.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header.component';
 import { AppRoutingModule } from '../../app-routing.module';
 import { SocialModule } from '../social/social.module';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [HeaderComponent],
-  imports: [CommonModule, AppRoutingModule, SocialModule],
+  imports: [CommonModule, AppRoutingModule, SocialModule, MatButtonModule, MatIconModule],
   exports: [HeaderComponent],
 })
 export class HeaderModule {}
