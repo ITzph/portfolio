6cfd9e555f4f07a97797b26bea6f3db401f10dcd
carlogino <carloginocatapang@gmail.com>
2020-09-06 20:51:43 +0800

    Added auth token for every request

--- apps/webapp/src/app/interceptors/http-header.interceptor.ts
+++ apps/webapp/src/app/interceptors/http-header.interceptor.ts
@@ -1,14 +1,23 @@
 import { Injectable } from '@angular/core';
 import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+import { take } from 'rxjs/operators';
 
 @Injectable()
 export class HttpHeaderInterceptor implements HttpInterceptor {
-  constructor() {}
+  constructor(private readonly authService: AuthService) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    let authToken = '';
+
+    this.authService
+      .getToken()
+      .pipe(take(1))
+      .subscribe((token) => (authToken = token));
+
     request = request.clone({
-      headers: request.headers.set('Authorization', 'Bearer ' + 'whatever token'),
+      headers: request.headers.set('Authorization', `Bearer ${authToken}`),
     });
 
     request = request.clone({ headers: request.headers.set('x-auth', 'random token') });
