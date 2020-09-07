e396d81db8975205c6ea01bda164b4d13b113eb4
carlogino <carloginocatapang@gmail.com>
2020-09-07 21:47:25 +0800

    Used memes module in admin module

--- apps/webapp/src/app/pages/admin/admin.module.ts
+++ apps/webapp/src/app/pages/admin/admin.module.ts
@@ -3,9 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
+import { MemesComponent } from './memes/memes.component';
+import { MemesModule } from '../memes/memes.module';
 
 @NgModule({
-  declarations: [AdminComponent],
-  imports: [CommonModule, AdminRoutingModule],
+  declarations: [AdminComponent, MemesComponent],
+  imports: [CommonModule, AdminRoutingModule, MemesModule],
+  exports: [AdminComponent, MemesComponent],
 })
 export class AdminModule {}
