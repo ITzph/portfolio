94cbce390b2e9094a04302e50c6933453d6571aa
carlogino <carloginocatapang@gmail.com>
2020-09-07 21:14:49 +0800

    Created a common layout

--- apps/webapp/src/app/app-routing.module.ts
+++ apps/webapp/src/app/app-routing.module.ts
@@ -1,36 +1,6 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { CanLoadAuthenticatedGuard } from './guards/can-load-authenticated.guard';
-import { CanActivateAuthenticatedGuard } from './guards/can-activate-authenticated.guard';
-
-const routes: Routes = [
-  {
-    path: 'auth',
-    loadChildren: () => import('./pages/auth/auth.module').then((m) => m.AuthModule),
-  },
-  {
-    path: 'profile',
-    loadChildren: () => import('./pages/profile/profile.module').then((m) => m.ProfileModule),
-  },
-  {
-    path: 'memes',
-    loadChildren: () => import('./pages/memes/memes.module').then((m) => m.MemesModule),
-  },
-  {
-    path: 'resume',
-    loadChildren: () => import('./pages/resume/resume.module').then((m) => m.ResumeModule),
-  },
-  {
-    path: 'admin',
-    loadChildren: () => import('./pages/admin/admin.module').then((m) => m.AdminModule),
-    canLoad: [CanLoadAuthenticatedGuard],
-  },
-  {
-    path: '',
-    redirectTo: '/profile',
-    pathMatch: 'full',
-  },
-];
+import { RouterModule } from '@angular/router';
+import { routes } from './routes';
 
 @NgModule({
   imports: [
