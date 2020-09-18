1811025a44a24b8c767781925204bf725bdcc0d5
carlogino <carloginocatapang@gmail.com>
2020-09-18 21:27:14 +0800

    Updated unit tests batch 4

--- apps/webapp/src/app/interceptors/http-header.interceptor.spec.ts
+++ apps/webapp/src/app/interceptors/http-header.interceptor.spec.ts
@@ -1,10 +1,13 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { HttpHeaderInterceptor } from './http-header.interceptor';
 
 describe('HttpHeaderInterceptor', () => {
   beforeEach(() =>
     TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule, RouterTestingModule],
       providers: [HttpHeaderInterceptor],
     }),
   );
