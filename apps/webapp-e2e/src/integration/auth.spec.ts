84e168064c2d95515d0c6dc71f1fa4ed34b33523
carlogino <carloginocatapang@gmail.com>
2020-09-19 19:12:41 +0800

    Added e2e test for successful login

--- apps/webapp-e2e/src/integration/auth.spec.ts
+++ apps/webapp-e2e/src/integration/auth.spec.ts
@@ -1,14 +1,21 @@
-import { getInputByName } from '../support/app.po';
+import { getInputByName, getButtonByName } from '../support/app.po';
 describe('webapp authentication', () => {
   before(() => {
     cy.exec('npm run seed-database', { timeout: 20000 });
   });
 
-  beforeEach(() => cy.visit('/'));
+  beforeEach(() => cy.visit('/auth'));
 
   it('should contain credentials field in loging page', () => {
-    cy.visit('/auth');
     getInputByName('username').should('be.visible');
     getInputByName('password').should('be.visible');
+  });
+
+  it('should loging when valid credentials is used', () => {
+    getInputByName('username').type('carlo');
+    getInputByName('password').type('password');
+    getButtonByName('login').click();
+
+    cy.location('pathname').should('eq', '/profile');
   });
 });
