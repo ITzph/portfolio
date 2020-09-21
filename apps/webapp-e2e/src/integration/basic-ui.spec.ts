9719a3c57e00419df9ff902808d3305b0d496c91
carlogino <carloginocatapang@gmail.com>
2020-09-21 19:54:40 +0800

    Added test for checking social handlers

--- apps/webapp-e2e/src/integration/basic-ui.spec.ts
+++ apps/webapp-e2e/src/integration/basic-ui.spec.ts
@@ -10,4 +10,13 @@ describe('webapp basic ui', () => {
     cy.get('main#app-root').scrollTo('bottom');
     cy.get('.main-header--pinned').should('exist');
   });
+
+  it('should contain social handlers', () => {
+    cy.visit('/');
+
+    cy.get('.social').should('have.length', 6);
+
+    cy.get('header.top-bar').find('p.name').should('have.length', 3);
+    cy.get('footer.footer').find('p.name').should('have.length', 3);
+  });
 });
