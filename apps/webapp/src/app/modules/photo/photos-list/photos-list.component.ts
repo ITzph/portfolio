86e08ad346503c2a62c65f76a64589dcdaf2dbfc
carlogino <carloginocatapang@gmail.com>
2020-09-26 19:21:02 +0800

    Integrated ordering or images

--- apps/webapp/src/app/modules/photo/photos-list/photos-list.component.ts
+++ apps/webapp/src/app/modules/photo/photos-list/photos-list.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, ChangeDetectionStrategy, Input } from '@angular/core
 import { IImageMetadata } from '@portfolio/api-interfaces';
 import { MemesService } from '../../../pages/memes/memes.service';
 import { trackByIdOrIndex } from '../../../utils/tracker-by-id.util';
-import { OrderByEventProp } from '../photo.model';
+import { OrderByEventProp, ORDER } from '../photo.model';
 
 @Component({
   selector: 'portfolio-photos-list',
@@ -16,6 +16,8 @@ export class PhotosListComponent implements OnInit {
 
   filterValue = '';
   filterKey: keyof IImageMetadata = 'tags';
+  orderBy = 'title';
+  order = ORDER.DESC;
 
   currentPage = 0;
 
@@ -37,7 +39,13 @@ export class PhotosListComponent implements OnInit {
       : this.memes;
 
     return memes.sort((a, b) => {
-      return a.title.localeCompare(b.title);
+      if (this.order === ORDER.DESC) {
+        return a[this.orderBy].localeCompare(b[this.orderBy]);
+      } else if (this.order === ORDER.ASC) {
+        return b[this.orderBy].localeCompare(a[this.orderBy]);
+      } else {
+        throw { message: 'should not come to this point' };
+      }
     });
   }
 
@@ -48,7 +56,12 @@ export class PhotosListComponent implements OnInit {
     }
   }
 
-  onOrderChange(event: OrderByEventProp) {}
+  onOrderChange(event: OrderByEventProp) {
+    if (event) {
+      this.order = event.order;
+      this.orderBy = event.orderBy;
+    }
+  }
 
   constructor(private readonly memesService: MemesService) {}
 
