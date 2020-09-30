7db6b0e11d5e45057212f1053fdee99409408e0e
carlogino <carloginocatapang@gmail.com>
2020-09-30 20:43:32 +0800

    Created contact me component

---
+++ apps/webapp/src/app/modules/contact-me/contact-me.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'portfolio-contact-me',
+  templateUrl: './contact-me.component.html',
+  styleUrls: ['./contact-me.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ContactMeComponent implements OnInit {
+  contactDetailsForm = this.fb.group({
+    firstName: ['', [Validators.required]],
+    lastName: [''],
+    email: ['', [Validators.required, Validators.email]],
+    message: ['', [Validators.required, Validators.minLength(2)]],
+  });
+
+  constructor(private readonly fb: FormBuilder) {}
+
+  ngOnInit(): void {}
+}
