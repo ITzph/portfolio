bc8d65490bdb0bccbeb3e3878e382a211f411de5
carlogino <carloginocatapang@gmail.com>
2020-10-01 21:42:41 +0800

    Added snackbar message when email is sent

--- apps/webapp/src/app/modules/contact-me/contact-me.component.ts
+++ apps/webapp/src/app/modules/contact-me/contact-me.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { EmailService } from './email.service';
 
 @Component({
@@ -16,17 +17,32 @@ export class ContactMeComponent implements OnInit {
     message: ['', [Validators.required, Validators.minLength(2)]],
   });
 
-  constructor(private readonly fb: FormBuilder, private readonly emailService: EmailService) {}
+  constructor(
+    private readonly fb: FormBuilder,
+    private readonly emailService: EmailService,
+    private readonly snackBar: MatSnackBar,
+  ) {}
 
   ngOnInit(): void {}
 
   onSubmitHandler() {
     const formValue = this.contactDetailsForm.value;
 
-    this.emailService.sendContactMeMessage(formValue).subscribe((res) => {
-      if (res.result) {
-        this.contactDetailsForm.reset();
-      }
-    });
+    this.emailService.sendContactMeMessage(formValue).subscribe(
+      (res) => {
+        if (res.result) {
+          this.contactDetailsForm.reset();
+
+          this.snackBar.open(res.message, 'success', {
+            duration: 2000,
+          });
+        }
+      },
+      (err) => {
+        this.snackBar.open(err.error.message, 'success', {
+          duration: 2000,
+        });
+      },
+    );
   }
 }
