718e21aebb84768a64fcf445401e0230ad4a8be9
carlogino <carloginocatapang@gmail.com>
2020-10-14 19:56:31 +0800

    Added JWT guard when patching experience

--- apps/api/src/app/profile/experience/experience.controller.ts
+++ apps/api/src/app/profile/experience/experience.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Param, ParseIntPipe, Patch, Req } from '@nestjs/common';
+import { Controller, Param, ParseIntPipe, Patch, Req, UseGuards } from '@nestjs/common';
 import { ExperienceService } from './experience.service';
 import { Request } from 'express';
 import { IUserExperience } from '@portfolio/api-interfaces';
+import { JwtAuthGuard } from '../../auth/jwt-auth.guard';
 
 @Controller('experience')
 export class ExperienceController {
   constructor(private readonly experienceService: ExperienceService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Patch(':id')
   patchExperience(@Param('id', ParseIntPipe) id: number, @Req() req: Request) {
     const experience: Partial<IUserExperience> = req.body;
