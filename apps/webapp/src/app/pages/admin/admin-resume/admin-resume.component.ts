d7e23b722db6702a0365d92907f72954eae475e3
carlogino <carloginocatapang@gmail.com>
2020-10-14 20:57:35 +0800

    Moved experience related to own component

--- apps/webapp/src/app/pages/admin/admin-resume/admin-resume.component.ts
+++ apps/webapp/src/app/pages/admin/admin-resume/admin-resume.component.ts
@@ -1,13 +1,4 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { IUserExperience } from '@portfolio/api-interfaces';
-import { Observable } from 'rxjs';
-import { Store, select } from '@ngrx/store';
-import * as fromProfile from '../../../reducers/profile.reducer';
-import { getExperiences } from '../../../selectors/profile.selectors';
-import { map, withLatestFrom } from 'rxjs/operators';
-import { HttpClient } from '@angular/common/http';
-import { environment } from '../../../../environments/environment';
-import { updateProfile } from '../../../actions/profile.actions';
 
 @Component({
   selector: 'portfolio-admin-resume',
@@ -16,57 +7,5 @@ import { updateProfile } from '../../../actions/profile.actions';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AdminResumeComponent implements OnInit {
-  constructor(
-    private readonly profileStore: Store<fromProfile.State>,
-    private readonly http: HttpClient,
-  ) {}
-
-  experiences$: Observable<IUserExperience[]> = this.profileStore.pipe(select(getExperiences));
-
-  eventsToModify: string;
-  experienceToModify: IUserExperience;
-
-  onSelectExperience(experience: IUserExperience) {
-    if (!this.eventsToModify) {
-      this.eventsToModify = experience.events;
-      this.experienceToModify = experience;
-    }
-  }
-
-  onExperienceUpdate() {
-    const { experienceToModify } = this;
-    const updatedExperience: Partial<IUserExperience> = {
-      events: this.eventsToModify,
-      id: experienceToModify.id,
-    };
-
-    this.http
-      .patch<IUserExperience>(
-        `${environment.api}/experience/${experienceToModify.id}`,
-        updatedExperience,
-      )
-      .pipe(withLatestFrom(this.profileStore.pipe(select(getExperiences))))
-      .subscribe(([experience, experiences]) => {
-        this.profileStore.dispatch(
-          updateProfile({
-            profile: {
-              experiences: experiences.map((exp) => {
-                const newExperience: IUserExperience = {
-                  id: experience.id,
-                  events: experience.events,
-                  endDate: experience?.endDate ?? exp.endDate,
-                  startDate: experience?.startDate ?? exp.startDate,
-                  name: experience?.name ?? exp.name,
-                  role: experience?.role ?? exp.role,
-                };
-
-                return exp.id === experience.id ? newExperience : exp;
-              }),
-            },
-          }),
-        );
-      });
-  }
-
   ngOnInit(): void {}
 }
--- apps/webapp/src/app/pages/admin/admin-resume/experience/experience.component.ts
+++ apps/webapp/src/app/pages/admin/admin-resume/experience/experience.component.ts
@@ -1,4 +1,13 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { select, Store } from '@ngrx/store';
+import { IUserExperience } from '@portfolio/api-interfaces';
+import { getExperiences } from '../../../../selectors/profile.selectors';
+import { Observable } from 'rxjs';
+import * as fromProfile from '../../../../reducers/profile.reducer';
+import { withLatestFrom } from 'rxjs/operators';
+import { updateProfile } from '../../../../actions/profile.actions';
+import { environment } from '../../../../../environments/environment';
 
 @Component({
   selector: 'portfolio-admin-experience',
@@ -6,8 +15,56 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
   styleUrls: ['./experience.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AdminExperienceComponent implements OnInit {
-  constructor() {}
+export class AdminExperienceComponent {
+  constructor(
+    private readonly profileStore: Store<fromProfile.State>,
+    private readonly http: HttpClient,
+  ) {}
 
-  ngOnInit(): void {}
+  experiences$: Observable<IUserExperience[]> = this.profileStore.pipe(select(getExperiences));
+
+  eventsToModify: string;
+  experienceToModify: IUserExperience;
+
+  onSelectExperience(experience: IUserExperience) {
+    if (!this.eventsToModify) {
+      this.eventsToModify = experience.events;
+      this.experienceToModify = experience;
+    }
+  }
+
+  onExperienceUpdate() {
+    const { experienceToModify } = this;
+    const updatedExperience: Partial<IUserExperience> = {
+      events: this.eventsToModify,
+      id: experienceToModify.id,
+    };
+
+    this.http
+      .patch<IUserExperience>(
+        `${environment.api}/experience/${experienceToModify.id}`,
+        updatedExperience,
+      )
+      .pipe(withLatestFrom(this.profileStore.pipe(select(getExperiences))))
+      .subscribe(([experience, experiences]) => {
+        this.profileStore.dispatch(
+          updateProfile({
+            profile: {
+              experiences: experiences.map((exp) => {
+                const newExperience: IUserExperience = {
+                  id: experience.id,
+                  events: experience.events,
+                  endDate: experience?.endDate ?? exp.endDate,
+                  startDate: experience?.startDate ?? exp.startDate,
+                  name: experience?.name ?? exp.name,
+                  role: experience?.role ?? exp.role,
+                };
+
+                return exp.id === experience.id ? newExperience : exp;
+              }),
+            },
+          }),
+        );
+      });
+  }
 }
