112226b6e14263656aadc268acc2e6fae7e12766
carlogino <carloginocatapang@gmail.com>
2020-11-09 20:33:06 +0800

    Used material button for download link

--- apps/webapp/src/app/pages/files/files.module.ts
+++ apps/webapp/src/app/pages/files/files.module.ts
@@ -10,6 +10,7 @@ import { MatCardModule } from '@angular/material/card';
 import { MatTableModule } from '@angular/material/table';
 import { FileListComponent } from './file-list/file-list.component';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [FilesViewerComponent, FileListComponent],
@@ -22,6 +23,7 @@ import { MatIconModule } from '@angular/material/icon';
     MatCardModule,
     MatTableModule,
     MatIconModule,
+    MatButtonModule,
   ],
   exports: [FilesViewerComponent, FileListComponent],
 })
