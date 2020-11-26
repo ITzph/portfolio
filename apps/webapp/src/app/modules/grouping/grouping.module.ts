3406f75f5fec53f0a76b8b77f899077ce1f2227f
carlogino <carloginocatapang@gmail.com>
2020-11-26 19:10:58 +0800

    Changed from material to prime ng

--- apps/webapp/src/app/modules/grouping/grouping.module.ts
+++ apps/webapp/src/app/modules/grouping/grouping.module.ts
@@ -3,19 +3,19 @@ import { CommonModule } from '@angular/common';
 import { FilterByTagComponent } from './filter-by-property/filter-by-property.component';
 import { SortByPropertyComponent } from './sort-by-property/sort-by-property.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatIconModule } from '@angular/material/icon';
 import { InputTextModule } from 'primeng/inputtext';
 import { DropdownModule } from 'primeng/dropdown';
+import { TooltipModule } from 'primeng/tooltip';
 
 @NgModule({
   declarations: [FilterByTagComponent, SortByPropertyComponent],
   imports: [
     CommonModule,
     ReactiveFormsModule,
-    MatIconModule,
     InputTextModule,
     DropdownModule,
     FormsModule,
+    TooltipModule,
   ],
   exports: [FilterByTagComponent, SortByPropertyComponent],
 })
