3af4e50f369ac6212e4a14638d5fde70a512a7e7
carlogino <carloginocatapang@gmail.com>
2020-11-30 20:32:45 +0800

    Remove mat-card and icon in header module

--- apps/webapp/src/app/modules/header/header.module.ts
+++ apps/webapp/src/app/modules/header/header.module.ts
@@ -2,26 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './profile-header/header.component';
 import { SocialModule } from '../social/social.module';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
 import { routes } from '../../routes';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatDialogModule } from '@angular/material/dialog';
 import { AdminHeaderComponent } from './admin-header/admin-header.component';
 import { AngularSvgIconModule } from 'angular-svg-icon';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
   declarations: [HeaderComponent, AdminHeaderComponent],
   imports: [
     CommonModule,
     SocialModule,
-    MatButtonModule,
-    MatIconModule,
     MatSnackBarModule,
     MatDialogModule,
     RouterModule.forChild(routes),
     AngularSvgIconModule,
+    ButtonModule,
   ],
   exports: [HeaderComponent, AdminHeaderComponent, AngularSvgIconModule],
 })
