251a126d4f83faad9adf4677a453896cb70efb65
carlogino <carloginocatapang@gmail.com>
2020-12-05 22:24:47 +0800

    Migrated to primeNG

--- apps/webapp/src/app/pages/admin/admin-resume/admin-resume.module.ts
+++ apps/webapp/src/app/pages/admin/admin-resume/admin-resume.module.ts
@@ -5,20 +5,11 @@ import { AdminResumeRoutingModule } from './admin-resume-routing.module';
 import { AdminResumeComponent } from './admin-resume.component';
 import { QuillModule } from 'ngx-quill';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
 import { AdminExperienceComponent } from './experience/experience.component';
 import { AdminCertificationComponent } from './certification/certification.component';
 import { AdminSkillComponent } from './skill/skill.component';
-import { MatInputModule } from '@angular/material/input';
-import { MatCardModule } from '@angular/material/card';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatNativeDateModule } from '@angular/material/core';
 import { UpdateExperienceComponent } from './experience/update-experience/update-experience.component';
-import { MatIconModule } from '@angular/material/icon';
 import { UpdateSkillComponent } from './skill/update-skill/update-skill.component';
-import { MatRadioModule } from '@angular/material/radio';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { UpdateCertificationComponent } from './certification/update-certification/update-certification.component';
 import { CardModule } from 'primeng/card';
 import { ButtonModule } from 'primeng/button';
@@ -26,6 +17,7 @@ import { AutoCompleteModule } from 'primeng/autocomplete';
 import { InputTextModule } from 'primeng/inputtext';
 import { ToggleButtonModule } from 'primeng/togglebutton';
 import { CalendarModule } from 'primeng/calendar';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @NgModule({
   declarations: [
@@ -42,22 +34,14 @@ import { CalendarModule } from 'primeng/calendar';
     AdminResumeRoutingModule,
     QuillModule.forRoot(),
     FormsModule,
-    MatButtonModule,
-    MatInputModule,
     ReactiveFormsModule,
-    MatCardModule,
-    MatDatepickerModule,
-    MatNativeDateModule,
-    MatIconModule,
-    MatRadioModule,
-    MatCheckboxModule,
-    MatAutocompleteModule,
     CardModule,
     ButtonModule,
     AutoCompleteModule,
     InputTextModule,
     ToggleButtonModule,
     CalendarModule,
+    MatDialogModule,
   ],
   exports: [
     AdminResumeComponent,
